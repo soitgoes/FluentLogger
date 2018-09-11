ae80230046c79e357059a58b0dc44dcf96adb978
Brian <brian@theparkerbunch.com>
2018-09-10 19:36:10 -0500

    comment_to_nullLogger

--- FluentLogger/NullLogger.cs
+++ FluentLogger/NullLogger.cs
@@ -2,16 +2,22 @@
 
 namespace FluentLogger
 {
-    public class NullLogger: BaseLogger
+    public class NullLogger : BaseLogger
     {
         public NullLogger(LogLevel minLevel = LogLevel.Trace) : base(minLevel)
         {
         }
-        
+        /// <summary>
+        /// For Testing Only.  Does Nothing
+        /// </summary>
+        /// <param name="level">Error Level</param>
+        /// <param name="message">Message Details</param>
+        /// <param name="ex">Exception</param>
+        /// <param name="objectsToSerialize">Object_Of_Message</param>
         public override void Record(LogLevel level, string message, Exception ex = null, params object[] objectsToSerialize)
         {
             //Do nothing.  For Testing only
         }
-        
+
     }
 }
